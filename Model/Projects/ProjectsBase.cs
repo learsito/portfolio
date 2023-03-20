2df0da81ff31029c3704380a3165df73d092059c
Diego <ogeid_to@hotmail.com>
2023-03-20 02:45:26 -0700

    Components responsive-design adjusted. Added data logic with classes. Fixed visible bugs.

--- Model/Projects/ProjectsBase.cs
+++ Model/Projects/ProjectsBase.cs
@@ -1,8 +1,10 @@
 namespace ProjectsPages;
 
 public class ProjectsBase {
+    public string Id { get; set; } = "DefaultId";
     public string Title { get; set; } = "Project title";
     public string Image { get; set; } = "default.png";
     public string Description { get; set; } = "There should be a description here...";
-    public string Href { get; set; } ="Projects";
+    public string Href { get; set; } = "Projects";
+    public bool NewTab { get; set; } = false;
 }
---
+++ Model/Technologies/TechnologiesBase.cs
@@ -0,0 +1,9 @@
+namespace Technologies;
+
+public class TechnologiesBase {
+    public string Id { get; set; } = "DefaultId";
+    public string Href { get; set; } = "/learsito/";
+    public string Image { get; set; } = "default.png";
+    public string Title { get; set; } = "Default Title";
+    public string Description { get; set; } = "There sould be a description here...";
+}
