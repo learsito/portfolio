cc44855beaa2cf995d926b56262c7119e016b962
Diego <ogeid_to@hotmail.com>
2023-03-19 21:34:06 -0700

    Initial commit

---
+++ Model/Projects/ProjectsBase.cs
@@ -0,0 +1,8 @@
+namespace ProjectsPages;
+
+public class ProjectsBase {
+    public string Title { get; set; } = "Project title";
+    public string Image { get; set; } = "default.png";
+    public string Description { get; set; } = "There should be a description here...";
+    public string Href { get; set; } ="Projects";
+}
